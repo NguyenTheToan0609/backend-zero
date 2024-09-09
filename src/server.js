f835508276dbba630ab662c72c445802edb077f6
NguyenTheToan0609 <thetoan0609@gmail.com>
2024-09-09 13:05:43 +0700

    Sử Dụng MySQL với Node.JS

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 require("dotenv").config();
 const configEngnie = require("./config/viewEngine");
 const webRoute = require("./routes/web");
-
 const app = express();
+const mysql = require("mysql2");
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 
@@ -12,6 +12,24 @@ configEngnie(app);
 
 //khai báo route
 app.use("/", webRoute);
+
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "123456",
+  database: "hoidanit",
+});
+
+connection.query(
+  "SELECT *FROM Users",
+
+  function (err, results, fields) {
+    console.log(">>>results", results); // results contains rows returned by server
+    console.log(">>>fields", fields); // fields contains extra meta data about results, if available
+  }
+);
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
