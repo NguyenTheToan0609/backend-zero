2eeea5985b932a055e23b1dd0052c816c5c2bfaf
NguyenTheToan0609 <thetoan0609@gmail.com>
2024-09-09 21:45:21 +0700

    Connection Cost - Tạo Mới Connection

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,17 @@
+const { json } = require("express");
+const connection = require("../config/database");
+
 const getHomePage = (req, res) => {
   res.render("sample.ejs");
 };
 
 const getABC = (req, res) => {
-  res.send("Check ABC");
+  let users = [];
+  connection.query("SELECT *FROM Users", function (err, results, fields) {
+    users = results;
+    console.log(">>>results", results); // results contains rows returned by server
+    res.send(JSON.stringify(users));
+  });
 };
 
 module.exports = {
