e7e11ae31c1cf1bd375565cc3b34c24715c76524
NguyenTheToan0609 <thetoan0609@gmail.com>
2024-09-10 18:38:50 +0700

    Design Form Add New User

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,7 +2,7 @@ const { json } = require("express");
 const connection = require("../config/database");
 
 const getHomePage = (req, res) => {
-  res.render("sample.ejs");
+  return res.render("home.ejs");
 };
 
 const getABC = (req, res) => {
